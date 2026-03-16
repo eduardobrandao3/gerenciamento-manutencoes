fa5089cb9946a51e1b143ece50867f0b1d1cdc85
Eduardo Brandao <eduardobrandao0311@gmail.com>
2026-03-16 17:42:23 -0300

    feat: adicionando criacao de usuario

---
+++ src/controllers/usuario/CreateUserController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { CreateUserService } from "../../services/usuario/CreateUserService";
+import { UserRequest } from "../../models/interfaces/usuario/UserRequest";
+
+class CreateUserController{
+  async handle(req: Request, resp: Response){
+    const {cpf, nome, email, telefone, senha}: UserRequest = req.body
+    const createUserService = new CreateUserService();
+
+    // ve se passou arquivo de foto
+    const foto = req.file?.filename as string
+
+
+    const usuario = await createUserService.execute({cpf, nome, email, telefone, senha, foto})
+    
+    return resp.json(usuario)
+  }
+}
+
+export { CreateUserController }
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import { Router, Request, Response } from "express";
+import multer from "multer";
+import uploadConfig from "./config/multer"
+import { CreateUserController } from "./controllers/usuario/CreateUserController";
 
 const router = Router();
+const upload = multer(uploadConfig.upload("./tmp"))
 
 router.get("/teste", (req: Request, resp: Response) => {
   return resp.json({ok: true})
 });
+
+// ROTAS DE USUÁRIO
+router.post("/usuarios", upload.single("foto"), new CreateUserController().handle)
 
 export { router }
